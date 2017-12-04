0b8d1ee0fd7f59e01d1a0b326cd93742699682fa
Andres Leon <aleon1220@gmail.com>
2017-12-04 17:33:07 +1300

    configuring mail with correct functionality
    functions correction with email functionality

--- php/contact.php
+++ php/contact.php
@@ -1,6 +1,6 @@
 <?php
 
-	//$to = 'info@themewagon.com';  // please change this email id
+	$to = 'raj@easyasweb.co.nz';// please change this email id
 	
 	$errors = array();
 	// print_r($_POST);
@@ -46,7 +46,7 @@
 	$message = $_POST['message'];
 	$from = $email;
 	$subject = 'Contact Form : Easy As Web';
-	$to = 'aleon1220@gmail.com';
+	$to = 'raj@easyasweb.co.nz';
 	$body = "From: $name\n Phone: $phone\n E-Mail: $email\n Message:\n $message";
 
 
